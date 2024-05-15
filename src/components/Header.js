20d250fe7e37df7747d31d61f628e7c69d51ab67
Hasanat902 <abulhasanat902@gmail.com>
2024-05-15 11:58:51 +0600

    implement next auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,55 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
-    return (
-        <div className="shadow-sm border-b sticky top-0 z-30 p-3 bg-white">
-            <div className="flex justify-between items-center max-w-6xl mx-auto">
-                {/* logo */}
-                <Link href="/" className="hidden lg:inline-flex">
-                    <Image src='/Instagram_logo_black.webp' width={96} height={96} alt="instagram logo" />
-                </Link>
-                <Link href="/" className="lg:hidden">
-                    <Image src='/800px-Instagram_logo_2016.webp' width={40} height={40} alt="instagram logo" />
-                </Link>
+  const { data: session } = useSession();
+  console.log(session);
 
-                {/* search input */}
-                <input type="text" placeholder="Search" className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]" />
+  return (
+    <div className="shadow-sm border-b sticky top-0 z-30 p-3 bg-white">
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
+        {/* logo */}
+        <Link href="/" className="hidden lg:inline-flex">
+          <Image
+            src="/Instagram_logo_black.webp"
+            width={96}
+            height={96}
+            alt="instagram logo"
+          />
+        </Link>
+        <Link href="/" className="lg:hidden">
+          <Image
+            src="/800px-Instagram_logo_2016.webp"
+            width={40}
+            height={40}
+            alt="instagram logo"
+          />
+        </Link>
 
-                {/* menu items */}
-                <button className="text-sm font-semibold text-blue-500">Log In</button>
-            </div>
-        </div>
-    );
+        {/* search input */}
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]"
+        />
+
+        {/* menu items */}
+        {session ? (
+          <img src={session.user.image} alt={session.user.name} className="w-10 h-10 rounded-full cursor-pointer" onClick={signOut} />
+        ) : (
+          <button
+            onClick={signIn}
+            className="text-sm font-semibold text-blue-500"
+          >
+            Log In
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
