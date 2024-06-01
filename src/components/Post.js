f1968f278bc70731850d996402ae2b3341c71c45
Hasanat902 <abulhasanat902@gmail.com>
2024-06-01 12:15:46 +0600

    added like functionality to post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HiOutlineDotsVertical } from 'react-icons/hi';
+import LikeSection from './LikeSection';
 
 const Post = ({post}) => {
     return (
@@ -10,6 +11,7 @@ const Post = ({post}) => {
                 <HiOutlineDotsVertical className='h-5 cursor-pointer' />
             </div>
             <img src={post.image} alt={post.caption} className='object-cover w-full' />
+            <LikeSection id={post.id} />
             <p className='p-5 truncate'>
                 <span className='font-bold mr-2'>{post.username}</span>
                 {post.caption}
